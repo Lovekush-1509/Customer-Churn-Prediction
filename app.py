2ba6ecc93b7c582f3c4437a5218ae922eb536708
lovekush <lovekush.ug22@nsut.ac.in>
2026-02-28 23:58:17 +0530

    live action

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import pandas as pd
+from predict import predictForBackend
+import csv
+
+app = Flask(__name__)
+
+
+# app.run(debug=True)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello jii, server running</p>"
+
+# customerID,gender,SeniorCitizen,Partner,Dependents,tenure,PhoneService,MultipleLines,InternetService,OnlineSecurity,OnlineBackup,DeviceProtection,TechSupport,StreamingTV,StreamingMovies,Contract,PaperlessBilling,PaymentMethod,MonthlyCharges,TotalCharges
+@app.route('/pre',methods = ['POST'])
+def predict_churn():
+    if 'file' not in request.files: return "No File Found", 400
+    print("file found")
+    file = request.files['file']
+    filename = 'data/test.csv'
+    f = pd.read_csv(file);
+    f.to_csv(filename,index=False)
+
+    predictForBackend(filename)
+
+    path = 'data/predictions_output.csv'
+    df = pd.read_csv(path)
+    jsn = df.to_json(orient='records')
+    return jsn
--- predict.py
+++ predict.py
@@ -6,46 +6,86 @@ import joblib
 import pandas as pd
 import sys
 
-# Check for command line argument
-if len(sys.argv) != 2:
-    print("Usage: python predict.py <path_to_csv_file>")
-    sys.exit(1)
+# # Check for command line argument
+# if len(sys.argv) != 2:
+#     print("Usage: python predict.py <path_to_csv_file>")
+#     sys.exit(1)
 
-csv_path = sys.argv[1]
+# csv_path = sys.argv[1]
 
-# Load saved files
-model = joblib.load("models/churn_model.pkl")
-scaler = joblib.load("models/scaler.pkl")
-encoders = joblib.load("models/encoders.pkl")
+# # Load saved files
+# model = joblib.load("models/churn_model.pkl")
+# scaler = joblib.load("models/scaler.pkl")
+# encoders = joblib.load("models/encoders.pkl")
 
-# Load new customer data
-new_data = pd.read_csv(csv_path)
+# # Load new customer data
+# new_data = pd.read_csv(csv_path)
 
-# Preprocess new data
-if "customerID" in new_data.columns:
-    new_data.drop("customerID", axis=1, inplace=True)
+# # Preprocess new data
+# if "customerID" in new_data.columns:
+#     new_data.drop("customerID", axis=1, inplace=True)
 
-for column in new_data.columns:
-    if column in encoders:
-        # Make sure all categories exist in encoder
-        new_data[column] = new_data[column].map(
-            lambda x: x if x in encoders[column].classes_ else encoders[column].classes_[0]
-        )
-        new_data[column] = encoders[column].transform(new_data[column])
+# for column in new_data.columns:
+#     if column in encoders:
+#         # Make sure all categories exist in encoder
+#         new_data[column] = new_data[column].map(
+#             lambda x: x if x in encoders[column].classes_ else encoders[column].classes_[0]
+#         )
+#         new_data[column] = encoders[column].transform(new_data[column])
 
-# Scale features
-new_data_scaled = scaler.transform(new_data)
+# # Scale features
+# new_data_scaled = scaler.transform(new_data)
 
-# Predict
-predictions = model.predict(new_data_scaled)
+# # Predict
+# predictions = model.predict(new_data_scaled)
 
-# Map results
-result_map = {1: "Customer going to be Churn", 0: "Customer going to be Stay"}
-new_data["Churn_Prediction"] = [result_map[p] for p in predictions]
+# # Map results
+# result_map = {1: "Customer going to be Churn", 0: "Customer going to be Stay"}
+# new_data["Churn_Prediction"] = [result_map[p] for p in predictions]
 
-# Show results
-print(new_data)
+# # Show results
+# print(new_data)
 
-# Optionally save predictions
-new_data.to_csv("data/predictions_output.csv", index=False)
-print("Predictions saved to predictions_output.csv")
+# # Optionally save predictions
+# new_data.to_csv("data/predictions_output.csv", index=False)
+# print("Predictions saved to predictions_output.csv")
+
+
+
+def predictForBackend(csv_path):
+    # Load saved files
+    model = joblib.load("models/churn_model.pkl")
+    scaler = joblib.load("models/scaler.pkl")
+    encoders = joblib.load("models/encoders.pkl")
+
+    # Load new customer data
+    new_data = pd.read_csv(csv_path)
+
+    # Preprocess new data
+    if "customerID" in new_data.columns:
+        new_data.drop("customerID", axis=1, inplace=True)
+
+    for column in new_data.columns:
+        if column in encoders:
+            # Make sure all categories exist in encoder
+            new_data[column] = new_data[column].map(
+                lambda x: x if x in encoders[column].classes_ else encoders[column].classes_[0]
+            )
+            new_data[column] = encoders[column].transform(new_data[column])
+
+    # Scale features
+    new_data_scaled = scaler.transform(new_data)
+
+    # Predict
+    predictions = model.predict(new_data_scaled)
+
+    # Map results
+    result_map = {1: "Customer going to Churn", 0: "Customer going to Stay"}
+    new_data["Churn_Prediction"] = [result_map[p] for p in predictions]
+
+    # Show results
+    print(new_data)
+
+    # Optionally save predictions
+    new_data.to_csv("data/predictions_output.csv", index=False)
+    print("Predictions saved to predictions_output.csv")
