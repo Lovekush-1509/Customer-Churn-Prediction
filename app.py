f69f28baa6c6b250435262c83b8a13f943d8b950
lovekush <lovekush.ug22@nsut.ac.in>
2026-03-01 00:19:54 +0530

    live action

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request
 import pandas as pd
 from predict import predictForBackend
-import csv
+import os
+
 
 app = Flask(__name__)
 
 
 # app.run(debug=True)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 @app.route("/")
 def hello_world():
