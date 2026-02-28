c8998dcdcbbeb745e2bfc15afa3f01afe7757277
lovekush <lovekush.ug22@nsut.ac.in>
2026-03-01 00:27:34 +0530

    live action

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # app.run(debug=True)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", debug=True)
 
 @app.route("/")
 def hello_world():
